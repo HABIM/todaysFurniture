d515b943594c3e88fa11ec8b0dd30b4d890446d2
meyou1004 <mey5u1004@gmail.com>
2020-03-11 18:57:48 +0900

    [성일이] 테스트

---
+++ src/Meyou1004.java
@@ -0,0 +1,19 @@
+public class Meyou1004 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("성일테스트");
+		System.out.println("야일~~~");
+		System.out.println("코일~~~");
+		System.out.println("코건야~~~지");
+		System.out.println("성일테스트");
+		System.out.println("야일~~~");
+		System.out.println("코일~~~");
+		System.out.println("코건야~~~지");
+		System.out.println("성일테스트");
+		System.out.println("야일~~~");
+		System.out.println("코일~~~");
+		System.out.println("코건야~~~지");
+	}
+
+}
