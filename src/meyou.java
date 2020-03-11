2d93fddd1ef4a99a7f74301e137eb6350a9080b3
daniel <mey5u1004@gmail.com>
2020-03-11 17:59:51 +0900

    [성일이]테스트

---
+++ src/meyou.java
@@ -0,0 +1,12 @@
+
+public class meyou {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub0
+		System.out.println("성일테스트");
+		System.out.println("야일~~~");
+		System.out.println("코일~~~");
+		System.out.println("코건야~~~지");
+	}
+
+}
