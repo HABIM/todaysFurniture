e977acccba7f468c73a2b7b4fe9c58da5c6c0461
rlagkqls97 <rlagkqls97@gmail.com>
2020-03-11 17:03:39 +0900

    [하빔이] 수정

--- src/habim.java
+++ src/habim.java
@@ -4,7 +4,9 @@ public class habim {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub0
-System.out.println("하빔이 테스트");
+System.out.println("하빔이 테스트1");
+System.out.println("하빔이 테스트2");
+
 	}
 
 }
